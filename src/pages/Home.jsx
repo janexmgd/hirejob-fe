71a2e90577a8b0c3c0109e9ab20454ae04c900dc
janexmgd <janexmgd@gmail.com>
2025-10-08 12:36:41 +0700

    fixing responsive at search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,18 +7,27 @@ export default function Home() {
   return (
     <div className='flex flex-col min-h-screen w-full px-8 md:px-16 bg-[#F6F7F8]'>
       <Navbar />
-      <div className='w-full mt-4 md:mt-8 flex flex-row gap-2 items-center bg-white text-gray-500'>
-        <div className='flex flex-grow flex-row items-center border-0 border-r-2 border-gray-300 pr-5'>
+      <div className='w-full mt-4 md:mt-8 flex flex-col md:flex-row gap-2 items-stretch bg-white text-gray-500 p-2 rounded-md shadow-sm'>
+        {/* Input + Icon */}
+        <div className='flex flex-row items-center border border-gray-300 rounded-md flex-grow'>
           <input
             type='text'
-            placeholder='search'
-            className='flex-grow p-3 focus:outline-none'
+            placeholder='Search...'
+            className='flex-grow p-3 focus:outline-none rounded-l-md'
           />
-          <FaSearch className='text-gray-400' />
+          <FaSearch className='text-gray-400 mr-3' />
         </div>
-        <button className='p-1'>sort</button>
 
-        <button className='p-2 bg-purple-600 text-white'>Search</button>
+        {/* Tombol sort dan search */}
+        <div className='flex flex-row md:flex-row gap-2'>
+          <button className='flex justify-center items-center p-2 border border-gray-300 rounded-md md:w-auto w-full'>
+            Sort
+          </button>
+
+          <button className='flex justify-center items-center p-2 bg-purple-600 text-white rounded-md md:w-auto w-full'>
+            Search
+          </button>
+        </div>
       </div>
       <section className='bg-white mt-4 md:mt-8 gap-3 flex flex-col'>
         <HomeCard />
